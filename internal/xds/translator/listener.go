39df5500e70554896229cb2478c5d93540444f8f
Huabing Zhao <zhaohuabing@gmail.com>
2024-11-30 22:01:40 +0800

    [release/v1.2] Cherry pick IPv6 support to v1.2.3 (#4819)
    * xds: use IPv4Compat on ready server (#4798)
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    (cherry picked from commit 9f9de74a4a90ec6439918404fdd29cb7d2d89e96)
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>
    
    * xds: use `::1`  if IPFamily is IPv6 on admin server (#4801)
    
    xds: use  if IPFamily is IPv6
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    (cherry picked from commit 17e932c7bd7ed62d3fa91d4f1afd176a69bd83eb)
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>
    
    * listen on ipv4 addresses by default (#4817)
    
    * listen on `::` if the listener IPFamily is `IPv6` or `DualStack`
    * Set `ipv4_compat` to `true` if `IPFamily` is `DualStack`
    
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    (cherry picked from commit d1a8c4720315d5feb80c066643c463845f4b2ec7)
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>
    
    ---------
    
    Signed-off-by: zirain <zirain2009@gmail.com>
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>
    Signed-off-by: Arko Dasgupta <arko@tetrate.io>
    Co-authored-by: zirain <zirain2009@gmail.com>
    Co-authored-by: Arko Dasgupta <arkodg@users.noreply.github.com>

--- internal/xds/translator/listener.go
+++ internal/xds/translator/listener.go
@@ -210,7 +210,6 @@ func buildXdsTCPListener(
 					PortSpecifier: &corev3.SocketAddress_PortValue{
 						PortValue: port,
 					},
-					Ipv4Compat: true,
 				},
 			},
 		},
@@ -248,7 +247,6 @@ func buildXdsQuicListener(name, address string, port uint32, ipFamily *egv1a1.IP
 					PortSpecifier: &corev3.SocketAddress_PortValue{
 						PortValue: port,
 					},
-					Ipv4Compat: true,
 				},
 			},
 		},
@@ -904,7 +902,6 @@ func buildXdsUDPListener(clusterName string, udpListener *ir.UDPListener, access
 					PortSpecifier: &corev3.SocketAddress_PortValue{
 						PortValue: udpListener.Port,
 					},
-					Ipv4Compat: true,
 				},
 			},
 		},
