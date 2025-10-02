b74dedb07830d9d002d1e0c3a5d52c42c08b0ffb
Logan Davis <logan.davis@goteleport.com>
2025-10-02 10:37:34 -0500

    Update testdata and update to proxy protocol settings

--- internal/gatewayapi/route.go
+++ internal/gatewayapi/route.go
@@ -1092,7 +1092,7 @@ func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resour
 
 				annotations := listener.gateway.GetAnnotations()
 				if v := annotations[AnnotationGatewayDownstreamProxyProtocol]; strings.ToLower(v) == "true" {
-					irListener.EnableProxyProtocol = true
+					irListener.ProxyProtocol = &ir.ProxyProtocolSettings{}
 				}
 
 				irListener.Routes = append(irListener.Routes, irRoute)
