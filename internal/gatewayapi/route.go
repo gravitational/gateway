085fd4c2cbbc044d744f5099c37aa945e5a44af3
Logan Davis <logan.davis@goteleport.com>
2025-10-07 09:15:40 -0500

    gocritic fixes

--- internal/gatewayapi/route.go
+++ internal/gatewayapi/route.go
@@ -973,7 +973,7 @@ func (t *Translator) ProcessTLSRoutes(tlsRoutes []*gwapiv1a2.TLSRoute, gateways 
 
 func teleportGetProxyProtocol(route *TLSRouteContext) *ir.ProxyProtocol {
 	annotations := route.GetAnnotations()
-	if v := annotations[AnnotationUpstreamProxyProtocol]; strings.ToLower(v) == "true" {
+	if v := annotations[AnnotationUpstreamProxyProtocol]; strings.EqualFold(v, "true") {
 		return ptr.To(ir.ProxyProtocol{
 			Version: ir.ProxyProtocolVersionV2,
 		})
@@ -1091,7 +1091,7 @@ func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resour
 				irRoute.CircuitBreaker = teleportGetCircuitBreaker(tlsRoute)
 
 				annotations := listener.gateway.GetAnnotations()
-				if v := annotations[AnnotationGatewayDownstreamProxyProtocol]; strings.ToLower(v) == "true" {
+				if v := annotations[AnnotationGatewayDownstreamProxyProtocol]; strings.EqualFold(v, "true") {
 					irListener.ProxyProtocol = &ir.ProxyProtocolSettings{}
 				}
 
