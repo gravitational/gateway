9745ae7cbd7d3544af5b722be69f67dfee001898
Logan Davis <logan.davis@goteleport.com>
2025-10-07 09:15:40 -0500

    gocritic fixes

--- internal/provider/kubernetes/predicates.go
+++ internal/provider/kubernetes/predicates.go
@@ -669,7 +669,8 @@ func (r *gatewayAPIReconciler) hasRouteWithEndpointRouting(nsName *types.Namespa
 		r.log.Error(err, "failed to find associated HTTPRoutes")
 		return false
 	}
-	for _, route := range httpRouteList.Items {
+	for i := range httpRouteList.Items {
+		route := &httpRouteList.Items[i]
 		if r.hasEndpointRouting(route.Namespace, route.Spec.CommonRouteSpec) {
 			return true
 		}
@@ -683,7 +684,8 @@ func (r *gatewayAPIReconciler) hasRouteWithEndpointRouting(nsName *types.Namespa
 			r.log.Error(err, "failed to find associated GRPCRoutes")
 			return false
 		}
-		for _, route := range grpcRouteList.Items {
+		for i := range grpcRouteList.Items {
+			route := &grpcRouteList.Items[i]
 			if r.hasEndpointRouting(route.Namespace, route.Spec.CommonRouteSpec) {
 				return true
 			}
@@ -698,7 +700,8 @@ func (r *gatewayAPIReconciler) hasRouteWithEndpointRouting(nsName *types.Namespa
 			r.log.Error(err, "failed to find associated TLSRoutes")
 			return false
 		}
-		for _, route := range tlsRouteList.Items {
+		for i := range tlsRouteList.Items {
+			route := &tlsRouteList.Items[i]
 			if r.hasEndpointRouting(route.Namespace, route.Spec.CommonRouteSpec) {
 				return true
 			}
@@ -713,7 +716,8 @@ func (r *gatewayAPIReconciler) hasRouteWithEndpointRouting(nsName *types.Namespa
 			r.log.Error(err, "failed to find associated TCPRoutes")
 			return false
 		}
-		for _, route := range tcpRouteList.Items {
+		for i := range tcpRouteList.Items {
+			route := &tcpRouteList.Items[i]
 			if r.hasEndpointRouting(route.Namespace, route.Spec.CommonRouteSpec) {
 				return true
 			}
@@ -728,7 +732,8 @@ func (r *gatewayAPIReconciler) hasRouteWithEndpointRouting(nsName *types.Namespa
 			r.log.Error(err, "failed to find associated UDPRoutes")
 			return false
 		}
-		for _, route := range udpRouteList.Items {
+		for i := range udpRouteList.Items {
+			route := &udpRouteList.Items[i]
 			if r.hasEndpointRouting(route.Namespace, route.Spec.CommonRouteSpec) {
 				return true
 			}
