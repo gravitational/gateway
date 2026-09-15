873a8be30e707639da4f9a68fa0ed58c363df4cd
Logan Davis <logan.davis@goteleport.com>
2026-09-15 11:17:17 -0500

    teleport: annotations for ProxyV2 and MaxConnections (#68)

--- internal/gatewayapi/route.go
+++ internal/gatewayapi/route.go
@@ -40,6 +40,13 @@ const (
 	HTTPRequestTimeout = "15s"
 	// egPrefix is a prefix of annotation keys that are processed by Envoy Gateway
 	egPrefix = "gateway.envoyproxy.io/"
+	// AnnotationUpstreamProxyProtocol enables proxy protocol for a given route. Currently
+	// only TLSRoutes support this annotation. The value is expected to be set to "true",
+	// case-insensitive, to enable proxy protocol. All other values will be ignored.
+	AnnotationUpstreamProxyProtocol = "cloud.teleport.dev/upstream-proxy-protocol"
+	// AnnotationUpstreamMaxConnections specifies the max upstream connections for a given
+	// route. Currently only TLSRoutes support this annotation. The value must be a valid uint32.
+	AnnotationUpstreamMaxConnections = "cloud.teleport.dev/upstream-max-connections"
 )
 
 var (
@@ -2295,6 +2302,31 @@ func (t *Translator) ProcessTLSRoutes(tlsRoutes []*gwapiv1.TLSRoute, gateways []
 	return relevantTLSRoutes
 }
 
+func teleportGetProxyProtocol(route *TLSRouteContext) *ir.ProxyProtocol {
+	annotations := route.GetAnnotations()
+	if v := annotations[AnnotationUpstreamProxyProtocol]; strings.ToLower(v) == "true" {
+		return ptr.To(ir.ProxyProtocol{
+			Version: ir.ProxyProtocolVersionV2,
+		})
+	}
+	return nil
+}
+
+func teleportGetCircuitBreaker(route *TLSRouteContext) *ir.CircuitBreaker {
+	annotations := route.GetAnnotations()
+
+	if v := annotations[AnnotationUpstreamMaxConnections]; v != "" {
+		uInt, err := strconv.ParseUint(v, 10, 32)
+		if err == nil {
+			return ptr.To(ir.CircuitBreaker{
+				MaxConnections:     ptr.To(uint32(uInt)),
+				MaxPendingRequests: ptr.To(uint32(uInt)),
+			})
+		}
+	}
+	return nil
+}
+
 func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resources *resource.Resources, xdsIR resource.XdsIRMap) {
 	for _, parentRef := range tlsRoute.ParentRefs {
 
@@ -2466,6 +2498,10 @@ func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resour
 					),
 					Metadata: routeRuleMetadata,
 				}
+
+				irRoute.ProxyProtocol = teleportGetProxyProtocol(tlsRoute)
+				irRoute.CircuitBreaker = teleportGetCircuitBreaker(tlsRoute)
+
 				irListener.Routes = append(irListener.Routes, irRoute)
 			}
 		}
