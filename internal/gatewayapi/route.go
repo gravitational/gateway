894ba874862dbc2467462a1c906d3e3c5360207a
Logan Davis <logan.davis@goteleport.com>
2026-09-15 11:17:17 -0500

    teleport: annotations for ProxyV2 and MaxConnections (#68)

--- internal/gatewayapi/route.go
+++ internal/gatewayapi/route.go
@@ -42,6 +42,13 @@ const (
 	egPrefix = "gateway.envoyproxy.io/"
 	// AnnotationTLSRouteProtos specifies the ALPN protos matched by a TLSRoute.
 	AnnotationTLSRouteALPNProtocols = "cloud.teleport.dev/protos"
+	// AnnotationUpstreamProxyProtocol enables proxy protocol for a given route. Currently
+	// only TLSRoutes support this annotation. The value is expected to be set to "true",
+	// case-insensitive, to enable proxy protocol. All other values will be ignored.
+	AnnotationUpstreamProxyProtocol = "cloud.teleport.dev/upstream-proxy-protocol"
+	// AnnotationUpstreamMaxConnections specifies the max upstream connections for a given
+	// route. Currently only TLSRoutes support this annotation. The value must be a valid uint32.
+	AnnotationUpstreamMaxConnections = "cloud.teleport.dev/upstream-max-connections"
 )
 
 var (
@@ -2309,6 +2316,31 @@ func alpnProtocolsForRoute(tlsRoute *TLSRouteContext) []string {
 	return protocols
 }
 
+func teleportGetProxyProtocol(route *TLSRouteContext) *ir.ProxyProtocol {
+	annotations := route.GetAnnotations()
+	if v := annotations[AnnotationUpstreamProxyProtocol]; strings.ToLower(v) == "true" {
+		return ptr.To(ir.ProxyProtocol{
+			Version: ir.ProxyProtocolVersionV2,
+		})
+	}
+	return nil
+}
+
+func teleportGetCircuitBreaker(route *TLSRouteContext) *ir.CircuitBreaker {
+	annotations := route.GetAnnotations()
+
+	if v := annotations[AnnotationUpstreamMaxConnections]; v != "" {
+		uInt, err := strconv.ParseUint(v, 10, 32)
+		if err == nil {
+			return ptr.To(ir.CircuitBreaker{
+				MaxConnections:     ptr.To(uint32(uInt)),
+				MaxPendingRequests: ptr.To(uint32(uInt)),
+			})
+		}
+	}
+	return nil
+}
+
 func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resources *resource.Resources, xdsIR resource.XdsIRMap) {
 	// The annotation is set on the route, so it applies to every parentRef and listener below.
 	alpnProtocols := alpnProtocolsForRoute(tlsRoute)
@@ -2484,6 +2516,10 @@ func (t *Translator) processTLSRouteParentRefs(tlsRoute *TLSRouteContext, resour
 					),
 					Metadata: routeRuleMetadata,
 				}
+
+				irRoute.ProxyProtocol = teleportGetProxyProtocol(tlsRoute)
+				irRoute.CircuitBreaker = teleportGetCircuitBreaker(tlsRoute)
+
 				irListener.Routes = append(irListener.Routes, irRoute)
 			}
 		}
